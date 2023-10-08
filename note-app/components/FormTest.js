a59ce66e57376b218d2d74b7f475f59d4b783ce3
EdMorales2986 <ed.morales6892@gmail.com>
2023-10-08 15:01:55 -0400

    Implementation of Formik and Axios

--- note-app/components/FormTest.js
+++ note-app/components/FormTest.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function FormHeaader() {
   return (
     <View>
-      <Text>FormHeaader</Text>
+      <Text>FormHeader</Text>
     </View>
   );
 }
--- note-app/screens/loginPage.js
+++ note-app/screens/loginPage.js
@@ -8,10 +8,14 @@ import {
   Pressable,
   TouchableWithoutFeedback,
   Keyboard,
+  Button,
 } from "react-native";
 // import Ionicons from "@expo/vector-icons/Ionicons";
 import { AntDesign } from "@expo/vector-icons";
+
 import { loginStyles } from "../styles/globalStyles";
+import { Formik } from "formik";
+import axios from "axios";
 
 export default function LoginPage() {
   const [user, setUser] = useState("");
@@ -20,6 +24,21 @@ export default function LoginPage() {
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
+  };
+
+  const submitData = async (e) => {
+    // console.log(e.alias, e.password);
+    try {
+      // MAFER, para que axios funcione el segmento de la Ip lo tienes que sustituir
+      // con el que te tira Metro despues de correr 'npx expo start' o 'npm run start'
+      const http = await axios.post("http://192.168.222.211:4000/signin", {
+        alias: e.alias,
+        password: e.password,
+      });
+      console.log(http.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -49,46 +68,67 @@ export default function LoginPage() {
         >
           Bienvenido!
         </Text>
-        <TextInput
-          style={loginStyles.inputA}
-          placeholder="User Name"
-          placeholderTextColor={"#092C70"}
-          value={user}
-          onChangeText={(e) => setUser(e)}
-        />
-        <View style={loginStyles.passwordBox}>
-          <TextInput
-            style={loginStyles.inputB}
-            secureTextEntry={!showPassword}
-            placeholder="User Password"
-            placeholderTextColor={"#092C70"}
-            value={password}
-            maxLength={30}
-            onChangeText={(e) => setPassword(e)}
-          />
-          <Pressable
-            style={{
-              borderRadius: 100,
-              alignItems: "center",
-              justifyContent: "center",
-              right: 30,
-              zIndex: 2,
-            }}
-            onPress={toggleShowPassword}
-          >
-            <AntDesign
-              style={loginStyles.icon}
-              name="eyeo"
-              size={24}
-              color="black"
-            />
-          </Pressable>
-        </View>
-        <View>
-          <Pressable style={loginStyles.btnWrapper}>
-            <Text style={loginStyles.btnText}>Login</Text>
-          </Pressable>
-        </View>
+        <Formik
+          initialValues={{ alias: "", password: "" }}
+          onSubmit={(e) => submitData(e)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <TextInput
+                style={loginStyles.inputA}
+                name="alias"
+                placeholder="User Name"
+                placeholderTextColor={"#092C70"}
+                onChangeText={handleChange("alias")}
+                onBlur={handleBlur("alias")}
+                value={values.alias}
+              />
+              <View style={loginStyles.passwordBox}>
+                <TextInput
+                  style={loginStyles.inputB}
+                  name="password"
+                  secureTextEntry={!showPassword}
+                  placeholder="User Password"
+                  placeholderTextColor={"#092C70"}
+                  maxLength={30}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  value={values.password}
+                />
+                <Pressable
+                  style={{
+                    borderRadius: 100,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    right: 30,
+                    zIndex: 2,
+                  }}
+                  onPress={toggleShowPassword}
+                >
+                  <AntDesign
+                    style={loginStyles.icon}
+                    name="eyeo"
+                    size={24}
+                    color="black"
+                  />
+                </Pressable>
+              </View>
+              <View>
+                <Pressable
+                  style={loginStyles.btnWrapper}
+                  onPress={handleSubmit}
+                >
+                  <Text style={loginStyles.btnText}>Login</Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+        </Formik>
       </View>
     </TouchableWithoutFeedback>
   );
